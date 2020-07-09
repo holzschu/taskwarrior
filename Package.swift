f57b8af21955e55d2c23c9a24bbdfed0ef8a56ae
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-07-09 21:50:07 +0200

    Updated Info.plist for simulator packages

--- Package.swift
+++ Package.swift
@@ -13,17 +13,17 @@ let package = Package(
         .binaryTarget(
             name: "task",
             url: "https://github.com/holzschu/taskwarrior/releases/download/1.0/task.xcframework.zip",
-            checksum: "15f7aa56150014e36ba851d40c178613443ee0926e6d37d7cb4136c86113a742"
+            checksum: "8082f26ab6d1c994532c0cba4bd262e1d2415868d46bb511a566a7b788d6ec50"
         ),
         .binaryTarget(
             name: "lex",
             url: "https://github.com/holzschu/taskwarrior/releases/download/1.0/lex.xcframework.zip",
-            checksum: "f19f1f95ea27f6bdca2196e0647fccd801dad84f942d63ed2c430b46b8a66406"
+            checksum: "d816952e7cfd67f39e57693b21cc52016fcc79f96a1f58e118dcd88c2f52f8d1"
         ),
         .binaryTarget(
             name: "calc",
             url: "https://github.com/holzschu/taskwarrior/releases/download/1.0/calc.xcframework.zip",
-            checksum: "a57efcd637558891e72469fd91355a3a47adbe8435c926ef722591317878e608"
+            checksum: "65964fd9946d63a65cf14443276ed1518380b7cc97b1feb5d40ffd91e601bf61"
         )
     ]
 )
