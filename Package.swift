e8a7aeaf57656e1ce3741263df9ba3952587db78
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-08-25 18:12:41 +0200

    Updated packages to Xcode 12b5

--- Package.swift
+++ Package.swift
@@ -13,17 +13,17 @@ let package = Package(
         .binaryTarget(
             name: "task",
             url: "https://github.com/holzschu/taskwarrior/releases/download/1.0/task.xcframework.zip",
-            checksum: "8082f26ab6d1c994532c0cba4bd262e1d2415868d46bb511a566a7b788d6ec50"
+            checksum: "0d4c043bbc0b40a8654508af78b97b67c258bd263e5c3c4953225bca99f317a1"
         ),
         .binaryTarget(
             name: "lex",
             url: "https://github.com/holzschu/taskwarrior/releases/download/1.0/lex.xcframework.zip",
-            checksum: "d816952e7cfd67f39e57693b21cc52016fcc79f96a1f58e118dcd88c2f52f8d1"
+            checksum: "db670dfe2f1c2b36e73114cf86ecce1e8f6a59640255314c38568718de0a3786"
         ),
         .binaryTarget(
             name: "calc",
             url: "https://github.com/holzschu/taskwarrior/releases/download/1.0/calc.xcframework.zip",
-            checksum: "65964fd9946d63a65cf14443276ed1518380b7cc97b1feb5d40ffd91e601bf61"
+            checksum: "063c92e2a36349aedf520ef97a952f16353bf91ee712410183be8846647e343e"
         )
     ]
 )
