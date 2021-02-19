26586102b916b1d1cb61988a9f608e2d340ea4f8
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2021-02-19 11:47:10 +0100

    Updating compilation options

--- Package.swift
+++ Package.swift
@@ -13,17 +13,17 @@ let package = Package(
         .binaryTarget(
             name: "task",
             url: "https://github.com/holzschu/taskwarrior/releases/download/1.0/task.xcframework.zip",
-            checksum: "3fcc3b3fdd9e9247f9b08c1140343e790d38070e02a0a79edff43eb148abf043"
+            checksum: "ac6589d1e90ed5eb2a969d50a618f9c92da2a74305ef9f6caa2d8b4d36837b70"
         ),
         .binaryTarget(
             name: "lex",
             url: "https://github.com/holzschu/taskwarrior/releases/download/1.0/lex.xcframework.zip",
-            checksum: "d4a8e9d7519e4120ea12c18b50f652f3c8188d21c6cc066adf716869c159ad8b"
+            checksum: "08916648891bd2070e61c0c202dcd6eed1840981ffd6dc38ffd940b03e31939b"
         ),
         .binaryTarget(
             name: "calc",
             url: "https://github.com/holzschu/taskwarrior/releases/download/1.0/calc.xcframework.zip",
-            checksum: "1fba531bbea928b983e3d72d786bf6ca0a6ca5de043357738a3fb0a4ce1233e9"
+            checksum: "4732b1eaad5f9060faaea73b33e43fb5d59af884b3eff678df27e2abb9b98cf0"
         )
     ]
 )
